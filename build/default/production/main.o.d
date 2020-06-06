b29fd440ae690614eb3e18336542e4161d36c132
Ruannilton <ruannilton@gmail.com>
2020-06-06 04:00:19 -0300

    logica pronta

--- build/default/production/main.o.d
+++
@@ -1,9 +0,0 @@
-build/default/production/main.o:  \
-	F:/Projects/PIC/LCD_LIB.X/main.asm  \
-	F:/Projects/PIC/LCD_LIB.X/lcd.inc  \
-	F:/Projects/PIC/LCD_LIB.X/lib.inc  \
-	F:/Projects/PIC/LCD_LIB.X/input.inc  \
-	F:/Projects/PIC/LCD_LIB.X/display.inc  \
-	F:/Projects/PIC/LCD_LIB.X/tela_app.inc  \
-	F:/Projects/PIC/LCD_LIB.X/tela_game.inc  \
-
