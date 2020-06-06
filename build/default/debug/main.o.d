db9ddf3113a4272f85496bf5ce6a846b31b65e02
Ruannilton <ruannilton@gmail.com>
2020-06-06 01:31:57 -0300

    first commit

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,9 @@
+build/default/debug/main.o:  \
+	F:/Projects/PIC/LCD_LIB.X/main.asm  \
+	F:/Projects/PIC/LCD_LIB.X/lcd.inc  \
+	F:/Projects/PIC/LCD_LIB.X/lib.inc  \
+	F:/Projects/PIC/LCD_LIB.X/input.inc  \
+	F:/Projects/PIC/LCD_LIB.X/display.inc  \
+	F:/Projects/PIC/LCD_LIB.X/tela_app.inc  \
+	F:/Projects/PIC/LCD_LIB.X/tela_game.inc  \
+
